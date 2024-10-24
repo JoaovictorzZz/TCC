532f7e051cc05f0da412bbb3b5369448442e9ec8
jaoberz <133983758+leone5800@users.noreply.github.com>
2024-10-24 12:08:56 -0300

    estilizada nos detalhes

--- js/detalhe.js
+++ js/detalhe.js
@@ -1,3 +1,9 @@
+//
+//hamburger
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.body.classList.toggle('menu-active');
+});
+//
 const thumbs = document.querySelectorAll('.thumbs img');
 const mainImage = document.querySelector('.main-image');
 

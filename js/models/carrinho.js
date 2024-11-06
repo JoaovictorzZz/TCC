f8feb43fe70128ec69a0fb9dd1048822e20b9e67
Joao Victor Vieira <joaovconst@gmail.com>
2024-11-06 20:06:26 -0300

    pagina de carrinho estilizada
    unica function dele e para remover o item da carriola

--- js/models/carrinho.js
+++ js/models/carrinho.js
@@ -1,12 +1,11 @@
 //modelo do carrinho 
-function removeItem(button){
-    const cartItem =button.closest('.cart-item');
+function removeItem(button) {
+    // Remove o elemento pai do botão, que é o item do carrinho
+    const cartItem = button.parentElement;
     cartItem.remove();
-    alert('Item removido !')
-}
 
-function adicionarItem(button){
-    const cartAddItem = document.querySelector('.cart-item');
-    cartItem.add();
-    alert('Item adicionado ');
-}   
+    //pega elementos do html 
+    const summary = document.querySelector(".summary");
+    summary.querySelector(".total").textContent = "Total: R$ 0,00";
+    summary.querySelector(".discount-price").textContent = "à vista R$ 0,00 no PIX com 15% desconto";
+}

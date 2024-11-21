edf0c66569a3655056c0009cc5c3bdae6cc52a5b
Kauê Amatuzi <136829156+kaueAmatuzi@users.noreply.github.com>
2024-11-21 20:00:49 -0300

    ADD informações no JSON
    a

--- teste.js
+++ teste.js
@@ -62,26 +62,248 @@ const produtos = [
 ,
   {
     "id": 3,
-    "nome": "Parafusadeira Furadeira de Impacto Wesco 3/8 Carregador Bivolt",
-    "modelo": "WS2547",
+    "nome": "Esmerilhadeira Angular Corte Desbaste 900w Wesco 220v Ws4750",
+    "modelo": "WS4750",
     "marca": "Wesco",
-    "descricao": "Furadeira Parafusadeira de Impacto 3/8\" Bateria 12V Wesco WS2547 - Com 01 Bateria de 1.Ah + 01 Carregar",
+    "descricao": "A Esmerilhadeira Angular WS4750 da Wesco é uma ferramenta essencial para trabalhos de corte e desbaste em diferentes materiais. Equipada com uma trava do eixo para troca de disco, proporciona praticidade e segurança durante a substituição dos acessórios. Sua empunhadura auxiliar com 2 posições oferece versatilidade e conforto ao usuário, permitindo uma melhor adaptação às diferentes necessidades de trabalho. Com um corpo mais fino e ergonômico, proporciona manuseio confortável e preciso, reduzindo a fadiga durante o uso prolongado. Além disso, seu interruptor liga/desliga lateral garante um controle fácil e intuitivo da máquina, oferecendo praticidade e segurança durante a operação.",
     "especificacoes": [
       "Alimentação: Bateria 12V 1.5Ah Li-ion",
-      "Tensão: 12V",
-      "Rotações: 0-350 / 0-1350 RPM",
+      "Tensão: 220V",
+      "Rotações: 12.000 rpm",
       "Impacto: 0-5600 / 0-21600 IPM",
       "Torque: Posição de torque: 21 + 1 + 1; Torque máximo: 25Nm",
       "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
       "Motor: sem escovas"
     ],
     "itensAcompanha": [
-      "01 bateria 18V 2.0Ah",
-      "01 Carregador Bivolt Rápido",
-      "01 clip para cinto"
+      "01 capa de proteção",
+      "01 empunhadura auxiliar",
+      "01 chave para troca do disco"
     ],
     "garantia": "1 ano de garantia direto com o fabricante",
-    "preco": "483,90",
+    "preco": "R$ 289,52",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+,
+  {
+    "id": 4,
+    "nome": "Esmerilhadeira Angular 230mm 9 Ws4703 220v Wesco Cor Azul-turquesa Frequência 60 Hz",
+    "modelo": "WS4750",
+    "marca": "Wesco",
+    "descricao": "A Esmerilhadeira Angular 230mm/2350W da WESCO é uma ferramenta poderosa projetada para atender às demandas mais exigentes em trabalhos de corte e desbaste. Com um motor de 2350W de alta potência, esta esmerilhadeira oferece desempenho excepcional para uma variedade de aplicações industriais e domésticas.",
+    "especificacoes": [
+      "Alimentação: Elétrica",
+      "Tensão: 220 volts",
+      "Rotações: 6.500rpm",
+      "Impacto: 0-5600 / 0-21600 IPM",
+      "Torque: Posição de torque: 21 + 1 + 1; Torque máximo: 25Nm",
+      "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 esmerilhadeira Angular 230mm 220V",
+      "01 capa de proteção",
+      "01 chave para troca do disco",
+      "01 chave Allen"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 476,08",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+
+  ,
+  {
+    "id": 5,
+    "nome": "Martelete Perfurador Demolidor Sds Plus 4,5j 1050w Wesco",
+    "modelo": "WS4750",
+    "marca": "Wesco",
+    "descricao": "MARTELETE PERFURADOR/ROMPEDOR 26MM 1.050W WESCO WS3162K",
+    "especificacoes": [
+      "Alimentação: Elétrica",
+      "Tensão: 220 volts",
+      "Rotações: 1.200 rpm",
+      "Impacto: 0-4.700/ipm",
+      "Torque: Posição de torque: 21 + 1 + 1; Torque máximo: 25Nm",
+      "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 empunhadura auxiliar",
+      "01 limitador de profundidade",
+      "01 capa protetora para pó",
+      "01 chave",
+      "01 maleta plástica"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 629,90",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+
+  ,
+  {
+    "id": 6,
+    "nome": "Chave De Impacto 1/2 Brushless 18v Bivolt Ws2382 Wesco",
+    "modelo": "WS4750",
+    "marca": "Wesco",
+    "descricao": "A Chave de Impacto 1/2 a Bateria Brushless 18V Bivolt WS2382 da Wesco é uma ferramenta versátil que pode ser usada em uma variedade de aplicações, especialmente aquelas que envolvem aperto e desaperto de parafusos e porcas com alta potência de torque",
+    "especificacoes": [
+      "Alimentação: 18V 2.0Ah Li",
+      "Tensão: 220 volts",
+      "Rotações: 0-3.200 rpm",
+      "Impacto: 0-4.700/ipm",
+      "Torque: 300 Nm",
+      "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 baterias de 2.0Ah",
+      "01 carregador 60 min - bivolt",
+      "01 clip de cinto"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 951,82",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+
+  ,
+  {
+    "id": 7,
+    "nome": "Lixadeira profissional roto orbital Wesco Profissional WS4265 azul-turquesa 400W 220V",
+    "modelo": "WS4265",
+    "marca": "Wesco",
+    "descricao": "Com esta lixadeira, você conseguirá um trabalho de maior qualidade para seus clientes, pois ela permitirá que você realize o lixamento de diferentes materiais com o melhor resultado. Consiga detalhes exclusivos!",
+    "especificacoes": [
+      "Alimentação: Elétrica",
+      "Tensão: 220 volts",
+      "Rotações: 0-3.200 rpm",
+      "Impacto: 0-4.700/ipm",
+      "Torque: 300 Nm",
+      "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 baterias de 2.0Ah",
+      "01 carregador 60 min - bivolt",
+      "01 clip de cinto"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 297,89",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+
+  ,
+  {
+    "id": 8,
+    "nome": "Máquina solda inverter Touch 150 BV branco/azul 110V/220V Profissional Boxer",
+    "modelo": "TOUCH 150BV",
+    "marca": "Boxer",
+    "descricao": "O KIT Inversora de Solda 140A BiVolt com Função TIG Raspagem e Máscara de solda TOUCH 150 Boxer para soldar eletrodos até 3.25mm. Possui funções inteligentes, como Hot Start para uma abertura de arco mais fácil e Anti Stick para que os eletrodos que colaram à peça possam ser utilizados novamente. Solda eletrodo E6013, E7018, ferro fundido e Inox.",
+    "especificacoes": [
+      "Alimentação: Elétrica",
+      "Tensão: Elétrica Bivolt",
+      "Rotações: 0-3.200 rpm",
+      "Impacto: 0-4.700/ipm",
+      "Torque: 300 Nm",
+      "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 Cabo com garra negativa 300A",
+      "01 Cabo com porta eletrodo 200A",
+      "01 Alça Tiracolo",
+      "01 Máscara de solda com vidro escuro"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 1.008,17",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+
+  ,
+  {
+    "id": 9,
+    "nome": "Máquina de solda inverter Boxer Flama 201 BV azul e preta 127/220V",
+    "modelo": "TOUCH 150BV",
+    "marca": "Boxer",
+    "descricao": "Inversora de Solda BOXER FLAMA 161BV MMA/Tig Bivolt. A Máquina de Solda Inversora FLAMA 161 BV é a Inversora Ideal para o Serralheiro. Foi Projetada Para os Mais Exigentes Profissionais de Montagens Metalúrgicas.",
+    "especificacoes": [
+      "Alimentação: Elétrica",
+      "Tensão: 110/220V",
+      "Eletrodos: 110V - até 2.5 mm / 220V até 3,25mm",
+      "Peso: 4,9Kg",
+      "Engate: 9mm",
+      "Capacidades: Capacidade máxima de perfuração em aço: 8m; Capacidade máxima de perfuração em alvenaria: 8mm; Capacidade máxima de perfuração em madeira: 20mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 Cabo com garra negativa 300A",
+      "01 Cabo com porta eletrodo 200A",
+      "01 Alça Tiracolo",
+      "01 Máscara de solda com vidro escuro"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 1.206",
+    "parcelas": "Até 12x R$104,21",
+    "imagens": [
+      "",
+      "https://exemplo.com/imagem2.jpg",
+      "https://exemplo.com/imagem3.jpg"
+    ]
+  }
+
+  ,
+  {
+    "id": 10,
+    "nome": "Máquina de solda inverter Boxer Flama 161 BV azul e preta 110V/220V",
+    "modelo": "TOUCH 150BV",
+    "marca": "Boxer",
+    "descricao": "Inversora de Solda BOXER FLAMA 161BV MMA/Tig Bivolt. A Máquina de Solda Inversora FLAMA 161 BV é a Inversora Ideal para o Serralheiro. Foi Projetada Para os Mais Exigentes Profissionais de Montagens Metalúrgicas.",
+    "especificacoes": [
+      "Alimentação: Elétrica",
+      "Tensão: 110/220V",
+      "Eletrodos: 110V - até 2.5 mm / 220V até 3,25mm",
+      "Peso: 4,9Kg",
+      "Amperagem: 110 v - 110 a @ 45% 160A @ 60%",
+      "Engate: 9mm",
+      "Motor: sem escovas"
+    ],
+    "itensAcompanha": [
+      "01 Cabo com garra negativa 300A",
+      "01 Cabo com porta eletrodo 200A",
+      "01 Alça Tiracolo",
+      "01 Máscara de solda com vidro escuro"
+    ],
+    "garantia": "1 ano de garantia direto com o fabricante",
+    "preco": "R$ 738,90",
     "parcelas": "Até 12x R$104,21",
     "imagens": [
       "",
